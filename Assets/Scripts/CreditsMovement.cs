44efebc6b6fae628ac19d50f9d633438b4160fb2
oscarczernuszyn <u7282674@anu.edu.au>
2025-04-08 22:31:00 +1000

    boss aesthetics and credits

---
+++ Assets/Scripts/CreditsMovement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsMovement : MonoBehaviour
+{
+    void Update()
+    {
+        if (transform.position.y > -38.53)
+            transform.position -= Vector3.up * Time.deltaTime * 1.5f;
+    }
+}
